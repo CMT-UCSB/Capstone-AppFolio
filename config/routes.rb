cdc4857a46e77d05aba46ac59f123d6f70087124
Hill Xiong <xempst@gmail.com>
2020-10-29 16:55:01 -0700

    hx - added team page and modified controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'team/team'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
+
+  get '/team' => 'pages#team'
 end
