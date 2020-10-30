93ffb4459ab713d61e86a736f99168c75512d99a
kgusdorf <kgusdorf@gmail.com>
2020-10-29 17:08:33 -0700

    adjust account url

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
     def team
     end
+
+    def account
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'account/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'faq/index'
   root 'pages#home'
-
+  
   get '/team' => 'pages#team'
+  get '/account' => 'pages#account'
 end
