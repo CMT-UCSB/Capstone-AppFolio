34f65d1727f94fc54ae40b2597669b2f878764ac
Rain Wang <runyu@umail.ucsb.edu>
2020-10-29 18:06:37 -0700

    RW - removed faq Controller, changed /faq/index page to /faq

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,7 @@ class PagesController < ApplicationController
     
     def team
     end
+
+    def faq
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
   get "/about" => "pages#about"
-  get 'faq/index'
+  get '/faq' => 'pages#faq'
   get '/team' => 'pages#team'
 end
