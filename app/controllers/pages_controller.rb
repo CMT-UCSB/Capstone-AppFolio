21bd35ca33365c1c77e534f9aa06ce082b335919
Rain Wang <runyu@umail.ucsb.edu>
2020-11-05 16:30:26 -0800

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,16 +9,9 @@ class PagesController < ApplicationController
     def team
     end
 
-<<<<<<< HEAD
-    def account
-    end
-
-    def faq
-=======
     def faq
     end
     
     def account
->>>>>>> 0d838f7bdfc981f81beb2496915bd1b72a2607bf
     end
 end
