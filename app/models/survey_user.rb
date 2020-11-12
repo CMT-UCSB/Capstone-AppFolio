da11f9a7241d84ae419bdd210f3522408bef977e
Rain Wang <runyu@umail.ucsb.edu>
2020-11-12 14:32:52 -0800

    fix changing survey_id problem

--- app/models/survey_user.rb
+++ app/models/survey_user.rb
@@ -1,11 +1,6 @@
 class SurveyUser < ApplicationRecord
 
-  # def initialize
-  #   super
-  #   self.survey_id = SecureRandom.uuid
-  # end
-
-  after_initialize :generate_survey_id
+  after_initialize :generate_survey_id, if: :new_record?
   private
   def generate_survey_id
     self.survey_id = SecureRandom.uuid
