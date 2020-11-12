b534c5cb4fbb2a1274c51005b724d0a77491b87e
Rain Wang <runyu@umail.ucsb.edu>
2020-11-12 14:21:15 -0800

    changed the survey_user model, fix SurveyUser.create error

--- app/models/survey_user.rb
+++ app/models/survey_user.rb
@@ -1,7 +1,13 @@
 class SurveyUser < ApplicationRecord
-  
-  def initialize
-    super
+
+  # def initialize
+  #   super
+  #   self.survey_id = SecureRandom.uuid
+  # end
+
+  after_initialize :generate_survey_id
+  private
+  def generate_survey_id
     self.survey_id = SecureRandom.uuid
   end
 
