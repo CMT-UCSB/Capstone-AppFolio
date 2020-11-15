a792ec0de8c26293acaf7944e017b21075efa4bf
Hill Xiong <xempst@gmail.com>
2020-11-15 15:32:36 -0800

    hx - added a notes page

---
+++ db/migrate/20201115225432_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :notes do |t|
+      t.string :date
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :notes, [:created_at]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_184154) do
+ActiveRecord::Schema.define(version: 2020_11_15_225432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,17 @@ ActiveRecord::Schema.define(version: 2020_11_11_184154) do
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_employees_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "date"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "survey_users", force: :cascade do |t|
@@ -46,4 +53,5 @@ ActiveRecord::Schema.define(version: 2020_11_11_184154) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notes", "users"
 end
