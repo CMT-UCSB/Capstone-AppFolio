68d3590b4e3f3483ac9be623e2c52fcd5a3e67ab
R-Kolhatkar <R_S_KOLHATKAR@ucsb.edu>
2020-11-27 11:58:42 -0800

    RK - added methods to SurveyController, changed SurveyController to SurveysController, and reconfigured survey creation form to store in surveys and questions tables upon clicking the Submit button

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,73 @@
+class SurveysController < ApplicationController
+    def show
+        @survey = Survey.find_by(id: params[:id])
+    
+        # if @survey.blank?
+        #   head :not_found
+        #   return
+        # end
+    end
+
+    # GET method to get all surveys from database
+    def index
+        @surveys = Survey.where(manager_id: current_manager.id)
+    end
+
+    # GET method for the new survey
+    def new
+        @survey = Survey.new
+    end
+
+    # POST method for processing form data
+    def create
+        @survey = Survey.new(survey_params)
+        @survey.manager_id = current_manager.id
+
+        respond_to do |format|
+            if @survey.save   
+                format.html { redirect_to '/account', notice: 'Survey was successfully created.' }
+                format.json { render :show, status: :created, location: @survey }
+            else   
+                format.html { render :new }
+                format.json { render json: @survey.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    # GET method for editing a survey based on id
+    def edit
+    end
+
+    # PUT method for updating in database a survey based on id
+    def update
+        respond_to do |format|
+            if @survey.update_attributes(survey_params)
+                format.html { redirect_to @survey, notice: 'Survey was successfully updated.' }
+                format.json { render :show, status: :ok, location: @survey }
+            else   
+                format.html { render :edit }
+                format.json { render json: @survey.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    # DELETE method for deleting a survey from database based on id
+    def destroy
+        @survey.destroy
+        respond_to do |format|
+            format.html { redirect_to survey_url, notice: 'Survey was successfully destroyed.' }
+            format.json { head :no_content }
+        end
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_survey
+      @survey = Survey.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def survey_params
+      params.require(:survey).permit(:interval, :time_of_day, :isAnonymous)
+    end
+end
