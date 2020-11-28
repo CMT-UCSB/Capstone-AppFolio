9506c287205833a24b9b28c5f8345bc0d13cd67d
kgusdorf <kgusdorf@gmail.com>
2020-11-27 23:49:31 -0800

    show questions on dashboard

--- db/migrate/20201120005347_create_questions.rb
+++ db/migrate/20201120005347_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.string :prompt
-      t.integer :type
+      t.integer :question_type
       t.references :survey
       t.timestamps
     end
