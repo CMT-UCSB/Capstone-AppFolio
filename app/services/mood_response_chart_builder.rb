32328d7f544a2211be1a001d76d36b8c74ef7e8a
Cambria <cambriatolsma@ucsb.edu>
2020-12-09 21:28:07 -0800

    this is dumb but it works

--- app/services/mood_response_chart_builder.rb
+++ app/services/mood_response_chart_builder.rb
@@ -5,7 +5,8 @@ class MoodResponseChartBuilder
 
     def pie_chart_data
         default_data = { "okay" => 0, "bad" => 0, "good" => 0 }
-        pie_chart_data = default_data.merge(MoodResponse.where(id: @mood_survey.questions.map(&:id)).group(:response).count)
+        pie_chart_data = default_data.merge(MoodResponse.where(question_id: @mood_survey.questions.map(&:id)).group(:response).count)
+        print "pie_chart_data"
         if pie_chart_data.values.any?(&:positive?)
             pie_chart_data
         end
