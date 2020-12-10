d907e0e57bdb4f890aef2c78baa2081dff7b1b4c
R-Kolhatkar <R_S_KOLHATKAR@ucsb.edu>
2020-12-10 10:18:12 -0800

    Revert "Revert "RK - added elapsed_time field to mood_responses and open_ended_responses tables""

--- app/controllers/open_ended_responses_controller.rb
+++ app/controllers/open_ended_responses_controller.rb
@@ -8,13 +8,20 @@ class OpenEndedResponsesController < ApplicationController
     if this_survey_response == nil
         @isFilled = false
     else
-        @isFilled = true
+        # check if if has been at least one week since a response has last been saved
+        weeks = ((DateTime.now.utc - this_survey_response.created_at) / (60 * 60 * 24 * 7))
+        this_survey_response.update(elapsed_time: weeks)
+        if this_survey_response.elapsed_time > 0
+          @isFilled = false
+        else
+          @isFilled = true
+        end
     end
 
     if @isFilled == false
-      OpenEndedResponse.create!(employee: employee, question: question, response: params[:survey][:response])
+        OpenEndedResponse.create!(employee: employee, question: question, response: params[:survey][:response])
     else
-      this_survey_response.update(response: params[:survey][:response])
+        this_survey_response.update(response: params[:survey][:response])
     end
     redirect_to surveys_success_path
   end
