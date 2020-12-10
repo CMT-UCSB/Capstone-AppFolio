112a7c20dccf91865b770c30ff26fb53210bf16b
R-Kolhatkar <r_s_kolhatkar@ucsb.edu>
2020-12-10 04:13:21 -0800

    RK - added elapsed_time field to mood_responses and open_ended_responses tables to store number of weeks elapsed since initial MoodResponse to question was created. If at least one week has passed, create a new MoodResponse; otherwise, update existing MoodResponse

--- db/migrate/20201120005425_create_open_ended_responses.rb
+++ db/migrate/20201120005425_create_open_ended_responses.rb
@@ -2,6 +2,7 @@ class CreateOpenEndedResponses < ActiveRecord::Migration[6.0]
   def change
     create_table :open_ended_responses do |t|
       t.string :response
+      t.integer :elapsed_weeks
       t.references :question
       t.references :employee, index: true, type: :uuid
       t.timestamps
