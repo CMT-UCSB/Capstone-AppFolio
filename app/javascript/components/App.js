b8fc8b64bada0910a4e700706d0f12748aaf10a7
Hill Xiong <junfengxiong@ucsb.edu>
2021-01-11 13:13:21 -0800

    hx - changing back the name scheme for reactHome

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import Home from './reactHome/ReactHome'
+import Home from './reactHome/reactHome'
 
 const App = () => {
     return (
