975af71d2f9203ef8315d9cd98c3379472524f6b
Cambria <cambriatolsma@ucsb.edu>
2021-01-19 09:29:47 -0800

    display three recent survey data

--- app/services/mood_response_chart_builder.rb
+++ app/services/mood_response_chart_builder.rb
@@ -4,11 +4,13 @@ class MoodResponseChartBuilder
     end
 
     def pie_chart_data
-        default_data = { "okay" => 0, "bad" => 0, "good" => 0 }
-        pie_chart_data = default_data.merge(MoodResponse.where(question_id: @mood_survey.questions.map(&:id)).group(:response).count)
-        print "pie_chart_data"
-        if pie_chart_data.values.any?(&:positive?)
-            pie_chart_data
+        if !@mood_survey.nil?
+            default_data = { "okay" => 0, "bad" => 0, "good" => 0 }
+            pie_chart_data = default_data.merge(MoodResponse.where(question_id: @mood_survey.questions.map(&:id)).group(:response).count)
+            print "pie_chart_data"
+            if pie_chart_data.values.any?(&:positive?)
+                pie_chart_data
+            end
         end
     end
 end
