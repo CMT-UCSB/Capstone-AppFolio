fee48c7f06ac224d43f0705c81e68b3698c58bac
Hill Xiong <junfengxiong@ucsb.edu>
2021-01-28 16:54:00 -0800

    hx - sentiment analysis can now return only the score and magnitude through their representing print function

--- app/services/google_nlp_sentiment.rb
+++ app/services/google_nlp_sentiment.rb
@@ -1,9 +1,6 @@
 class GoogleNlpSentiment
     def initialize(input_text:)
         @text = input_text
-    end
-
-    def printResult
         require "google/cloud/language"
 
         # set google credential
@@ -11,26 +8,21 @@ class GoogleNlpSentiment
         Google::Cloud::Language.configure do |config|
             config.credentials = JSON.parse(ENV.fetch('GOOGLE_API_CREDS'))
         end
-        
+
         language = Google::Cloud::Language.language_service
 
-        text_content = "Hello Worlds!"
-
-        # text_content is the string which will be analyze by the NLP
+        # text is the string which will be analyze by the NLP
         document = { content: @text, type: :PLAIN_TEXT }
         response = language.analyze_sentiment document: document
 
-        sentiment = response.document_sentiment
+        @sentiment = response.document_sentiment
+    end
 
-        puts "Overall document sentiment: (#{sentiment.score})"
-        puts "Sentence level sentiment:"
+    def printScore
+        @sentiment.score
+    end
 
-        sentences = response.sentences
-
-        sentences.each do |sentence|
-            sentiment = sentence.sentiment
-            puts "#{sentence.text.content}: (#{sentiment.score})"
-        end
-        
+    def printMagnitude
+        @sentiment.magnitude
     end
 end
