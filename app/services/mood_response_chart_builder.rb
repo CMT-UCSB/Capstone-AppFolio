fb88e0573d694bd77725fcf6a329506fd44634ea
Hill Xiong <xempst@gmail.com>
2021-02-27 18:30:08 -0800

    hx - survey result can now display which elapsed survey to look at

--- app/services/mood_response_chart_builder.rb
+++ app/services/mood_response_chart_builder.rb
@@ -1,12 +1,13 @@
 class MoodResponseChartBuilder
-    def initialize(question:)
+    def initialize(question:, elapsed:)
         @question = question
+        @elapsed = elapsed
     end
 
     def pie_chart_data
         if !@question.nil?
             default_data = { "terrible" => 0, "bad" => 0, "okay" => 0, "good" => 0, "amazing" => 0 }
-            pie_chart_data = default_data.merge(MoodResponse.where(question_id: @question.id).group(:response).count)
+            pie_chart_data = default_data.merge(MoodResponse.where(question_id: @question.id, elapsed: @elapsed).group(:response).count)
             print "pie_chart_data"
             if pie_chart_data.values.any?(&:positive?)
                 pie_chart_data
--- app/services/open_ended_response_chart_builder.rb
+++ app/services/open_ended_response_chart_builder.rb
@@ -1,12 +1,13 @@
 class OpenEndedResponseChartBuilder
-    def initialize(question:)
+    def initialize(question:, elapsed:)
         @question = question
+        @elapsed = elapsed
     end
 
     def pie_chart_data
         if !@question.nil?
             default_data = { "neutral" => 0, "mixed" => 0, "slightly_negative" => 0, "negative" => 0, "slightly_positive" => 0, "positive" => 0 }
-            pie_chart_data = default_data.merge(OpenEndedResponse.where(question_id: @question.id).group(:sentiment).count)
+            pie_chart_data = default_data.merge(OpenEndedResponse.where(question_id: @question.id, elapsed: @elapsed).group(:sentiment).count)
             print "pie_chart_data"
             if pie_chart_data.values.any?(&:positive?)
                 pie_chart_data
