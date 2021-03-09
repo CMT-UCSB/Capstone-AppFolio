67e49a391eb28ab892b4e85a7b98eae4dc75e98f
Rain Wang <runyu@umail.ucsb.edu>
2021-03-09 02:53:28 -0800

    try to fix modal for New Note, 3rd try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,9 +24,11 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-
+  
+  config.serve_static_assets = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
